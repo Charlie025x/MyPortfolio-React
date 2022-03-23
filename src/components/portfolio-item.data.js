88061c61312b64ab900ca516ad1e1af22d938f30
Charlie025x <charliealonso2001@gmail.com>
2022-03-23 13:04:01 -0500

    removed picture in picture project and added time-tracking-dashboard portfolio project

--- src/components/portfolio-item.data.js
+++ src/components/portfolio-item.data.js
@@ -1,44 +1,48 @@
 const portfolioItemsData = [
-    {
-        id: 1,
-        name: "CRWN Clothing",
-        thumbnailUrl: "assets/img/portfolio/portfolio-crwn-clothing.png",
-        imageCount: 4,
-        moreDetailsUrl: "/portfolio-details",
-        date: ": 27 August, 2021",
-        liveUrl: "https://crwn-live-charlie-2021.herokuapp.com/",
-        description: "An E-commerce clothing store built on a React environment. The page has subdirectories for each category of clothes sold, shopping cart feature, utilizes Stripe api for payment processing, and handles user authentication through Firebase."
-    },
-    {
-        id: 2,
-        name: "quote-generator",
-        thumbnailUrl: "assets/img/portfolio/react-quote-generator.png",
-        imageCount: 1,
-        moreDetailsUrl: "/portfolio-details",
-        date: ": 16 October, 2021",
-        liveUrl: "https://charlie-alonso-react-quote-generator.netlify.app/",
-        description: "Generates quotes using an api containing hundreds of random quotes. I learned how to fetch, and render, data from an api. Built with vanilla Javascript, then rebuilt using react."
-    },
-    {
-        id: 3,
-        name: "My Portfolio",
-        thumbnailUrl: "assets/img/portfolio/portfolio-website.png",
-        imageCount: 1,
-        moreDetailsUrl: "/portfolio-details",
-        date: ": 2 October, 2021",
-        liveUrl: "\\",
-        description: "My portfolio built with a bootstrapmade.com template, iPortfolio. This project uses React.js's props to dynamicly change the portfolio-details page depending on which project's link was clicked."
-    },
-    {
-        id: 4,
-        name: "Picture in Picture",
-        thumbnailUrl: "assets/img/portfolio/picture-in-picture.png",
-        imageCount: 1,
-        moreDetailsUrl: "/portfolio-details",
-        date: ": 12 November, 2021",
-        liveUrl: "https://charlie025x.github.io/Picture-in-Picture/",
-        description: "Utilizes the Screen Capture api to create a website that can create a picture in picture video stream of a pc screen, window, or even another chrome tab (works great for a tab playing a Youtube video!)."
-    },
+  {
+    id: 1,
+    name: "CRWN Clothing",
+    thumbnailUrl: "assets/img/portfolio/portfolio-crwn-clothing.png",
+    imageCount: 4,
+    moreDetailsUrl: "/portfolio-details",
+    date: ": 27 August, 2021",
+    liveUrl: "https://crwn-live-charlie-2021.herokuapp.com/",
+    description:
+      "An E-commerce clothing store built on a React environment. The page has subdirectories for each category of clothes sold, shopping cart feature, utilizes Stripe api for payment processing, and handles user authentication through Firebase.",
+  },
+  {
+    id: 2,
+    name: "quote-generator",
+    thumbnailUrl: "assets/img/portfolio/react-quote-generator.png",
+    imageCount: 1,
+    moreDetailsUrl: "/portfolio-details",
+    date: ": 16 October, 2021",
+    liveUrl: "https://charlie-alonso-react-quote-generator.netlify.app/",
+    description:
+      "Generates quotes using an api containing hundreds of random quotes. I learned how to fetch, and render, data from an api. Built with vanilla Javascript, then rebuilt using react.",
+  },
+  {
+    id: 3,
+    name: "My Portfolio",
+    thumbnailUrl: "assets/img/portfolio/portfolio-website.png",
+    imageCount: 1,
+    moreDetailsUrl: "/portfolio-details",
+    date: ": 2 October, 2021",
+    liveUrl: "\\",
+    description:
+      "My portfolio built with a bootstrapmade.com template, iPortfolio. This project uses React.js's props to dynamically change the portfolio-details page depending on which project's link was clicked.",
+  },
+  {
+    id: 4,
+    name: "Time Tracking Dashboard",
+    thumbnailUrl: "assets/img/portfolio/time-tracking-dashboard.png",
+    imageCount: 4,
+    moreDetailsUrl: "/portfolio-details",
+    date: ": 13 March, 2022",
+    liveUrl: "https://charlie025x.github.io/Time-tracking-dashboard/",
+    description:
+      "A profile dashboard that displays the amount of hours spent on certain activities, the dashboard also allows for viewing the hours spent on said activity in three different time frames (daily, weekly, and monthly). With React.js, I rendered a card for every category there was data for in my data.json file. Then I added onClick events on the time frame buttons to change the state of the active time frame accordingly, resulting in the cards rerendering to show the correct hours spent, as per data.json's data",
+  },
 ];
 
-export default portfolioItemsData
+export default portfolioItemsData;
