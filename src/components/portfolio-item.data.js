543b68eec3118700aa3fc7cad5388118036b5bc3
Charlie025x <charliealonso2001@gmail.com>
2022-03-25 00:00:58 -0500

    added tip-calculator portfolio

--- src/components/portfolio-item.data.js
+++ src/components/portfolio-item.data.js
@@ -43,6 +43,17 @@ const portfolioItemsData = [
     description:
       "A profile dashboard that displays the amount of hours spent on certain activities, the dashboard also allows for viewing the hours spent on said activity in three different time frames (daily, weekly, and monthly). With React.js, I rendered a card for every category there was data for in my data.json file. Then I added onClick events on the time frame buttons to change the state of the active time frame accordingly, resulting in the cards rerendering to show the correct hours spent, as per data.json's data",
   },
+  {
+    id: 5,
+    name: "Tip Calculator App",
+    thumbnailUrl: "assets/img/portfolio/tip-calculator.png",
+    imageCount: 4,
+    moreDetailsUrl: "/portfolio-details",
+    date: ": 20 March, 2022",
+    liveUrl: "https://charlie025x.github.io/Tip-calculator-app/",
+    description:
+      "This web application outputs the tip for a bill for different scenarios. The calculator allows the user to input their bill, tip percentage preference, and the amount of people the bill will be split between, so that the user can know what their tip should be (per person). Built with React.js and SCSS.",
+  },
 ];
 
 export default portfolioItemsData;
