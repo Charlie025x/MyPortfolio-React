f7bff6e6693a79364780aaacd18d2764a37c4b8a
Charlie025x <charliealonso2001@gmail.com>
2022-11-06 18:55:44 -0600

    added profile picture, typed items, and removed default readme

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
- 
+import React, { Component } from "react";
+
 class Hero extends Component {
-    render() {
-        return (
-            <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
-                <div className="hero-container" data-aos="fade-in">
-                    <h1>Charlie Alonso</h1>
-                    <p>I'm a <span className="typed" data-typed-items="Designer, Developer, Researcher"></span></p>
-                </div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section
+        id="hero"
+        className="d-flex flex-column justify-content-center align-items-center"
+      >
+        <div className="hero-container" data-aos="fade-in">
+          <h1>Charlie Alonso</h1>
+          <p>
+            I'm a{" "}
+            <span
+              className="typed"
+              data-typed-items="Designer, Developer, Researcher, Problem Solver, Engineer, Self Starter, Artist"
+            ></span>
+          </p>
+        </div>
+      </section>
+    );
+  }
 }
 export default Hero;
